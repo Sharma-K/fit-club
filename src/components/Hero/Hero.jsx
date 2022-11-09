3c7293416590b0a9a9735dc9a7cc66ab1ab925b3
Keshav Sharma <keshav2743@gmail.com>
2022-11-09 23:04:09 +0530

    initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../Header/Header';
+import './Hero.css'
+const Hero = () => {
+    return (
+        <div className='hero'>
+           <div className="left-h">
+            <Header />
+            <div className="the-best-ad">
+            <div></div>
+            <span>The best fitness club in the town</span>
+           </div>
+           </div>
+           <div className="right-h">Right Side</div>
+            </div>
+    );
+}
+
+export default Hero;
