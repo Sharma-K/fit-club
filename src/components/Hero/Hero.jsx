947bf4f0d11e86f2ab29fa88d15acb233b2ba29b
Keshav Sharma <keshav2743@gmail.com>
2022-11-10 23:09:21 +0530

    final changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,6 +6,7 @@ import hero_image_back from "../../assets/hero_image_back.png"
 import Heart from "../../assets/heart.png"
 import Calories from "../../assets/calories.png"
 import { motion } from 'framer-motion';
+import NumberCounter from 'number-counter';
 const Hero = () => {
 
     const mobile = window.innerWidth<=780?true:false;
@@ -43,15 +44,33 @@ const Hero = () => {
 
                 <div className="figures">
                     <div>
-                        <span>+140</span>
+                        <span>
+                            <NumberCounter
+                            end={140} 
+                            start={100}
+                             delay='3'
+                              preFix="+"/>
+                        </span>
                         <span>Expert Coaches</span>
                     </div>
                     <div>
-                        <span>+978</span>
+                        <span>
+                        <NumberCounter
+                            end={978} 
+                            start={800}
+                             delay='3'
+                              preFix="+"/>
+                        </span>
                         <span>Members joined</span>
                     </div>
                     <div>
-                        <span>+50</span>
+                        <span>
+                        <NumberCounter
+                            end={50} 
+                            start={0}
+                             delay='2'
+                              preFix="+"/>
+                        </span>
                         <span>fitness programs</span>
                     </div>
                 </div>
